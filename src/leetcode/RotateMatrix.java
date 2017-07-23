8809dab6d57527cbd873ad2b03e4fe9b7f3e0480
jknow <joeko1337@gmail.com>
2017-07-23 19:23:08 -0400

    Added simpler boundry-restricting solution

--- src/leetcode/RotateMatrix.java
+++ src/leetcode/RotateMatrix.java
@@ -1,8 +1,48 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class RotateMatrix {
 
-    public static void rotate(int[][] matrix) {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        if (matrix.length == 0 || matrix[0].length == 0) return Collections.emptyList();
+        List<Integer> ans = new ArrayList<Integer>(matrix.length*matrix[0].length);
+        
+        int iTop = 0;
+        int iBottom = matrix.length-1;
+        int jLeft = 0;
+        int jRight = matrix[0].length-1;
+        while (iTop <= iBottom && jLeft <= jRight) {
+            
+            for (int j = jLeft; j <= jRight; j++) ans.add(matrix[iTop][j]);
+            
+            iTop++;
+            
+            for (int i = iTop; i <= iBottom; i++) ans.add(matrix[i][jRight]);
+            
+            if (iTop > iBottom) break;
+            jRight--;
+            
+            for (int j = jRight; j >= jLeft; j--) {
+                ans.add(matrix[iBottom][j]);
+            }
+            
+            if (jLeft > jRight) break;
+            iBottom--;
+            
+            for (int i = iBottom; i >= iTop; i--) {
+                ans.add(matrix[i][jLeft]);
+            }
+            
+            jLeft++;
+        }
+        
+        return ans;
+    }
+
+    public static void rotateCTCI(int[][] matrix) {
        if (matrix.length == 0 || matrix.length == 1) return ;
         int size = matrix.length;
         
