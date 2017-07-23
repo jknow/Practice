48e8433349050a6c3273e2a4e745e5b5131cd2c2
jknow <joeko1337@gmail.com>
2017-07-23 19:15:38 -0400

    Added test cases

--- src/leetcode/RegexMatching.java
+++ src/leetcode/RegexMatching.java
@@ -3,7 +3,29 @@ package leetcode;
 public class RegexMatching {
 
 	public static void main(String[] args) {
-		System.out.println(isMatch("aab", "c*a*b"));
+		boolean result = true;
+		result &= isMatch("aa","a") == false;
+		System.out.println(result);
+		result &= isMatch("aa","aa") == true;
+		System.out.println(result);
+		result &= isMatch("aaa","aa") == false;
+		System.out.println(result);
+		result &= isMatch("aa", "a*") == true;
+		System.out.println(result);
+		result &= isMatch("aa", ".*") == true;
+		System.out.println(result);
+		result &= isMatch("ab", ".*") == true;
+		System.out.println(result);
+		result &= isMatch("aab", "c*a*b") == true;
+		System.out.println(result);
+		result &= isMatch("aab", "c*.*a*b.*") == true;
+		System.out.println(result);
+		result &= isMatch("aab", "c*.*a*bc") == false;
+		System.out.println(result);
+		result &= isMatch("ab", ".*c") == false;
+		System.out.println(result);
+		result &= isMatch("aaa", "a*a") == true;
+		System.out.println(result);
 	}
 
     public static boolean isMatch(String s, String p) {
