dd1e955546df4330f49e5067a9a8deedf00723f5
jknow <joeko1337@gmail.com>
2017-07-23 18:55:53 -0400

    Successful RegexMatching

--- src/leetcode/RegexMatching.java
+++ src/leetcode/RegexMatching.java
@@ -8,29 +8,25 @@ public class RegexMatching {
 
     public static boolean isMatch(String s, String p) {
         if (s == null || p == null) return false;
-        char[] in = s.toCharArray();
-        int inLen = in.length;
-        char[] re = p.toCharArray();
-        int reLen = re.length;
-        boolean ans = false;
-        
-        int g = 0;
-        for (int i = 0; i < reLen; i++) {
-            if (in[i] == re[g] || re[g] == '.') {
-                if (g+1 < reLen && re[g] == '*') {
-                    while (in[i] == re[g] || re[g] == '.') {
-                        
-                    }
-                }
-            }
-            if (in[i] != re[g]) {
-                if (g+1 < reLen && re[g] == '*') {
-                    g++;
-                }
-            }
-        }
-        
-        return ans;
+        return isMatchHelper(s.toCharArray(), p.toCharArray(), 0, 0);
+    }
+    
+    public static boolean isMatchHelper(char[] in, char[] re, int i, int g) {
+    	if (i == in.length && g == re.length) return true;
+    	if (g == re.length) return false;
+    	
+    	if (i < in.length && (in[i] == re[g] || re[g] == '.')) {
+    		if (g+1 < re.length && re[g+1] == '*') {
+    			return isMatchHelper(in, re, i+1, g) || isMatchHelper(in, re, i+1, g+2) || isMatchHelper(in, re, i, g+2);
+    		} else {
+    			return isMatchHelper(in, re, i+1, g+1);
+    		}
+    	} else if (g+1 < re.length && re[g+1] == '*') {
+			return isMatchHelper(in, re, i, g+2);
+    	} else {
+        	return false;
+    	}
+    	
     }
 
 }
